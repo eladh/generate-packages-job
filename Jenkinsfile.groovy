5b561445def5820e0f0eba77e0e0e417dfc93c4f
Elad Hirsch <hirsch.elad@gmail.com>
2019-05-26 18:30:41 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,7 +20,7 @@ podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
                                         -e 'ARTIFACTORY_USER=$USER' \
                                         -e 'ARTIFACTORY_PASSWORD=$PASSWORD' \
                                         -e 'ARTIFACTORY_REPO=$REPO_NAME' \
-                                        -e 'PACKAGES_CLONE_MAX_LEVEL=$PACKAGES_CLONE_MAX_LEVEL' \
+                                        -e 'PACKAGES_CLONE_MAX_LEVEL=$PACKAGE_CLONE_MAX_LEVEL' \
                                         -e 'PACKAGES_DUPLICATION_RATE=$PACKAGES_DUPLICATION_RATE' \
                                         -e 'PACKAGE_NUMBER=$NUM_OF_ARTIFACTS' \
                                         -e 'PACKAGE_SIZE_MIN=$PACKAGE_SIZE_MIN' \
@@ -39,7 +39,7 @@ void firstTimeInit() {
                         string(name: 'REPO_NAME', defaultValue: '' ,description: 'Please select target repo name',),
                         string(name: 'PACKAGE_SIZE_MIN', defaultValue: '' ,description: 'Please select min size',),
                         string(name: 'PACKAGE_SIZE_MAX', defaultValue: '' ,description: 'Please select max size',),
-                        string(name: 'PACKAGES_CLONE_MAX_LEVEL', defaultValue: '' ,description: 'Please select max clone level',),
+                        string(name: 'PACKAGE_CLONE_MAX_LEVEL', defaultValue: '' ,description: 'Please select max clone level',),
                         string(name: 'PACKAGES_DUPLICATION_RATE', defaultValue: '' ,description: 'Please select duplication rate',),
                         string(name: 'NUM_OF_ARTIFACTS', defaultValue: '' ,description: 'Please select num of artifacts to generate',),
                 ])
